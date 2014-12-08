44427abe9bd0c90c941aa1a14ab79b805fb98995
Aldrin Jerome Almacin <AJAlmacin@Aldrins-MacBook-Air.local>
2014-12-08 18:41:05 -0500

    Database setup for products

---
+++ app/database/migrations/2014_12_08_231135_create_products.php
@@ -0,0 +1,78 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProducts extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('products', function($table) {
+			$table->increments('id');
+			$table->decimal('price');
+			$table->decimal('original_price');
+			$table->string('image');
+			$table->integer('quantity');
+
+			$table->timestamps();
+			$table->softDeletes();
+		});
+
+		Schema::create('rims', function($table) {
+			$table->increments('id');
+			$table->string('material');
+			$table->string('size');
+			$table->string('bolt_pattern');
+
+			$table->integer('product_id')->unsigned();
+			$table->foreign('product_id')
+			->references('id')
+			->on('products')
+			->onDelete('cascade');
+
+			$table->timestamps();
+			$table->softDeletes();
+		});
+
+		Schema::create('tires', function($table) {
+			$table->increments('id');
+			$table->string('brand_name');
+			$table->text('description');
+			$table->string('size');
+			$table->string('model');
+
+			$table->integer('product_id')->unsigned();
+			$table->foreign('product_id')
+				->references('id')
+				->on('products')
+				->onDelete('cascade');
+
+			$table->integer('rim_id')->unsigned();
+			$table->foreign('rim_id')
+				->references('id')
+				->on('rims')
+				->onDelete('cascade');
+
+			$table->timestamps();
+			$table->softDeletes();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('rims');
+		Schema::drop('tires');
+		Schema::drop('products');
+	}
+
+}
