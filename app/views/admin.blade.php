5fc94863553dd27fa4e411261403f3516a8dd507
Aldrin Jerome Almacin <AJAlmacin@Aldrins-MacBook-Air.local>
2014-12-08 18:07:10 -0500

    Setup Roles

--- app/views/admin.blade.php
+++ app/views/admin.blade.php
@@ -3,6 +3,12 @@
 Admin Page
 @stop
 @section('content')
-<h1>Admin</h1>
 <a href="{{ URL::to('logout') }}">Logout</a>
+@if(Auth::user()->role == 'reader')
+<h1>Reader</h1>
+@elseif(Auth::user()->role == 'accounting')
+<h1>Accounting</h1>
+@elseif(Auth::user()->role == 'admin')
+<h1>Admin</h1>
+@endif
 @stop
