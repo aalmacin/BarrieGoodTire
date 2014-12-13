2698aca609b5d416412cd584f6b4b3b50f76e8bb
Aldrin Almacin <aldrinjerome19@gmail.com>
2014-12-13 17:06:51 -0500

    SEO fixes

--- app/views/layout.blade.php
+++ app/views/layout.blade.php
@@ -16,7 +16,7 @@
         <h1>Barrie Good Tire</h1>
       </div>
       <div class="well row">
-        <p>To place an order, email <a href="mailto:julien@barriegoodtire.com">julien@barriegoodtire.com</a> or call <a href="tel:17057171034">705-717-1034</a></p>
+        <p>To place an order or for your questions, email <a href="mailto:julien@barriegoodtire.com">julien@barriegoodtire.com</a> or call <a href="tel:17057171034">705-717-1034</a></p>
       </div>
       <div class="row">
         <div class="col-sm-3">
@@ -29,7 +29,7 @@
                   <span class="icon-bar"></span>
                   <span class="icon-bar"></span>
                 </button>
-                <span class="visible-xs navbar-brand">Sidebar menu</span>
+                <span class="visible-xs navbar-brand">Pages</span>
               </div>
               <div class="mainnav navbar-collapse collapse sidebar-navbar-collapse">
                 <ul class="nav navbar-nav">
